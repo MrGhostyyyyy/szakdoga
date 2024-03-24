4cbff39fd468b763f2bdce5feb739d22deaf3299
Marcell Sós <marcell.sos@dlabs.hu>
2024-03-24 19:50:00 +0100

    Basic restructure of htmls and other files + login page works so far.

--- gameserver/login/views.py
+++ gameserver/login/views.py
@@ -1,6 +1,18 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login
 
-# Create your views here.
 def index(request):
-    return HttpResponse("login needs work")
+    data = {"app_name": "login"}
+
+    if request.method == "POST":
+        username = request.POST.get('username')
+        pwd = request.POST.get('pwd')
+        user = authenticate(request, name=username, password=pwd)
+        if user is not None:
+            login(request, user)
+            return redirect("landing")
+        else:
+            data["error"] = "Hiba a bejelentkezésnél, próbáld újra!"
+            return render(request, "login/login.html", data)
+    else:
+        return render(request, "login/login.html", data)
